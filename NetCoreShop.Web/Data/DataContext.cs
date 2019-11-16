7f5e0a5135820aab23bbdf58122a4b7e9ef51afb
Amankeldi Kairbay <amankhair38@gmail.com>
2019-11-16 19:11:04 +0600

    Lessons 1-4.

---
+++ NetCoreShop.Web/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetCoreShop.Web.Data.Entities;
+
+namespace NetCoreShop.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+    }
+}
