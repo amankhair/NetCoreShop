34ec8fd555e60b3db053bc5f6715e9f58dcc767c
Amankeldi Kairbay <amankhair38@gmail.com>
2019-11-17 16:56:56 +0600

    Lessons 14-15. (Azure publish). Add Images.

---
+++ NetCoreShop.Web/Models/ProductViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using NetCoreShop.Web.Data.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace NetCoreShop.Web.Models
+{
+    public class ProductViewModel : Product
+    {
+        [Display(Name = "Image")]
+        public IFormFile ImageFile { get; set; }
+
+    }
+}
