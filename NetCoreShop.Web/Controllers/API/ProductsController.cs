d18b26033335afd52d68b76e21e39360430d0423
Amankeldi Kairbay <amankhair38@gmail.com>
2019-11-17 14:34:56 +0600

    Lesson 13. Add API.

---
+++ NetCoreShop.Web/Controllers/API/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreShop.Web.Data;
+
+namespace NetCoreShop.Web.Controllers.API
+{
+    [Route("api/[Controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository productRepository;
+        public ProductsController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return this.Ok(this.productRepository.GetAll());
+        }
+
+    }
+}
